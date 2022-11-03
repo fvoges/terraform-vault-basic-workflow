7157799cdf74a6d9e1f99cff45ce77c7006d2b04
Federico Voges <federico@hashicorp.com>
2022-11-03 12:59:01 +0000

    add group membership management

--- modules/terraform-vault-internal_group/main.tf
+++ modules/terraform-vault-internal_group/main.tf
@@ -1,19 +1,29 @@
 locals {
   group_name       = replace(lower(var.group_name), "/\\W/", "_")
   member_group_ids = var.member_group_ids == [] ? null : var.member_group_ids
+  namespace        = var.namespace
 }
 
 resource "vault_identity_group" "internal" {
   name                       = var.group_name
+  namespace                  = local.namespace
   type                       = "internal"
-  external_member_entity_ids = var.allow_external_member_entity_ids
-  metadata                   = var.group_metadata
-  member_group_ids           = local.member_group_ids
   external_policies          = true
+  external_member_entity_ids = true
+  external_member_group_ids  = true
+  metadata                   = var.group_metadata
 }
 
 resource "vault_identity_group_policies" "ns_admins" {
+  namespace = local.namespace
   policies  = var.vault_policies
   group_id  = vault_identity_group.internal.id
   exclusive = false
+}
+
+resource "vault_identity_group_member_group_ids" "group_members" {
+  namespace        = local.namespace
+  member_group_ids = local.member_group_ids
+  group_id         = vault_identity_group.internal.id
+  exclusive        = false
 }
